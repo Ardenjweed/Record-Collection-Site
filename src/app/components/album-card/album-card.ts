4ce38675f19ad9c2d7c64f543997930cee96a322
Arden Weed <ardenjweed@gmail.com>
2026-04-08 19:10:14 -0700

    Set up structure of site flow

---
+++ src/app/components/album-card/album-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+//Displays Individual Albums
+// HTML can be as simple as <img [src]="album.coverUrl" [alt]="album.title" class="img-fluid">
+
+@Component({
+  selector: 'app-album-card',
+  imports: [],
+  templateUrl: './album-card.html',
+  styleUrl: './album-card.css',
+})
+export class AlbumCard {}
---
+++ src/app/models/album.ts
@@ -0,0 +1,11 @@
+// Defines Album Meta Data STRUCTURE (Something like this)
+// export interface Album {
+//   id: number;
+//   title: string;
+//   artist: string;
+//   genre: string;
+//   year: number;
+//   coverUrl: string;
+//   label?: string;
+//   notes?: string;
+// }
