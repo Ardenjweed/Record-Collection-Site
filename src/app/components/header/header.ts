9e6acef604d74ce6ba043b6d2e99f25236f8f0de
Arden Weed <ardenjweed@gmail.com>
2026-04-15 16:14:25 -0700

    Edited Display button to be an icon

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { AlbumService } from '../../services/album';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSelectModule, MatCheckboxModule],
+  imports: [MatSelectModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatButtonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
